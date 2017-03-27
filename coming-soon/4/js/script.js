ddfc75a9cb401fdd7818cbf63d02595e97647c30
roferwoo <roferwoo@gmail.com>
2017-03-27 16:55:25 +0800

    add files

---
+++ coming-soon/4/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    /* ---- Countdown timer ---- */
+    $('#counter').countdown({
+        timestamp: (new Date(2017, 03, 01)).getTime()
+    });
+
+    /* ---- Animations ---- */
+    $('#links a').hover(
+        function () {
+            $(this).animate({left: 3}, 'fast');
+        },
+        function () {
+            $(this).animate({left: 0}, 'fast');
+        }
+    );
+
+    $('footer a').hover(
+        function () {
+            $(this).animate({top: 3}, 'fast');
+        },
+        function () {
+            $(this).animate({top: 0}, 'fast');
+        }
+    );
+});
